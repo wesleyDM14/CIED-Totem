421e730f41a493887a58ada4f876f35832895f0c
wesleyDM14 <wesleyfga@hotmail.com>
2025-05-23 16:45:16 -0300

    Add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Totem from "./pages/TotemSenha"
+import Totem from "./pages/TotemSenha";
+
+const expectedToken = import.meta.env.VITE_APP_SECRET_KEY;
 
 function App() {
-  return <Totem />
+  const urlParams = new URLSearchParams(window.location.search);
+  const receivedToken = urlParams.get("auth");
+
+  if (receivedToken !== expectedToken) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px", fontSize: "24px", color: "red" }}>
+        <h1>Acesso negado</h1>
+        <p>Token inválido ou ausente.</p>
+      </div>
+    );
+  }
+
+  return <Totem />;
 }
 
-export default App
+export default App;
